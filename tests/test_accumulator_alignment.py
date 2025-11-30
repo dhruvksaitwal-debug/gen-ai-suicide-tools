103e4ee8ddf5f318b622f958f87e1e7d97aab899
Kishor <kishor.saitwal@gmail.com>
2025-11-30 03:21:31 -0600

    Fixed accumulator_alignment unit test

--- tests/test_accumulator_alignment.py
+++ tests/test_accumulator_alignment.py
@@ -1,5 +1,5 @@
 import unittest
-from GenAI.answer_normalizer import AnswerAccumulator
+from answer_normalizer import AnswerAccumulator
 
 class TestAnswerAccumulatorAlignment(unittest.TestCase):
     def test_update_and_provenance(self):
